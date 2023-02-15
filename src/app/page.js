50d04290e924032927be55bea5d1ec19b33c6c32
Zbigniew Szalkowski <zbigniew.szalkowski@gmail.com>
2023-02-15 17:40:05 +0100

    install tailwind css

--- src/app/page.js
+++ src/app/page.js
@@ -3,5 +3,7 @@ import { Inter } from '@next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <h1 className='text-3xl text-teal-600 font-bold underline'>Hello world!</h1>
+  );
 }
