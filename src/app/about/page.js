96f00149ef5e5743e47eea78a356fa13d8adc164
Zbigniew Szalkowski <zbigniew.szalkowski@gmail.com>
2023-02-17 14:49:35 +0100

    create the about page

---
+++ src/app/about/page.js
@@ -0,0 +1,34 @@
+const About = () => {
+  return (
+    <div className='max-w-6xl mx-auto space-y-4 '>
+      <h1 className='text-2xl font-medium text-amber-600 '>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac orci
+        auctor nisi porttitor interdum ut sit amet nibh. Donec at gravida orci,
+        a bibendum enim. Aenean sit amet sagittis turpis. Aenean gravida lacus
+        mi, nec ornare justo rutrum rutrum. Donec neque lacus, facilisis id
+        augue in, consequat ultricies ipsum. Maecenas tincidunt convallis
+        bibendum. Proin vel egestas dolor. Nam luctus imperdiet risus, vel
+        volutpat ante fringilla eget. In aliquet finibus arcu. Pellentesque nec
+        scelerisque massa.
+      </p>
+      <p>
+        Pellentesque sit amet turpis maximus, malesuada dui vel, pharetra magna.
+        Cras eleifend justo lectus, quis feugiat magna dignissim id.
+        Pellentesque ac hendrerit dolor, dictum varius dui. Proin elit justo,
+        iaculis quis ante quis, tristique cursus odio. Donec lobortis dignissim
+        suscipit. Aliquam id pulvinar purus. Donec eros nunc, consectetur
+        blandit posuere lobortis, facilisis tempor nunc. Vestibulum urna libero,
+        ornare sit amet nisi et, finibus porta justo. Morbi placerat finibus
+        arcu vel porttitor. Vestibulum ultricies quam vulputate viverra
+        ullamcorper. Integer enim dui, blandit sed facilisis et, venenatis eget
+        risus. Donec sollicitudin tortor vel dolor porta pretium. In hac
+        habitasse platea dictumst. Sed nec mi sed leo lacinia tempus vel vitae
+        justo. Curabitur id venenatis nunc, consectetur suscipit neque. Maecenas
+        suscipit euismod sem sed tristique. Nunc ut leo a ex ultrices mattis.
+      </p>
+    </div>
+  );
+};
+
+export default About;
